c37ce977ce7aacd5289841b2bd97dd69bac38a40
King Pra <kingi.pra@gmail.com>
2019-06-02 20:46:21 -0500

    slides 2--I think

--- src/main/java/com/techtalentsouth/TechTalentBlog/BlogPost/BlogPostController.java
+++ src/main/java/com/techtalentsouth/TechTalentBlog/BlogPost/BlogPostController.java
@@ -1,5 +1,8 @@
 package com.techtalentsouth.TechTalentBlog.BlogPost;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,17 +14,26 @@ public class BlogPostController {
 
 	@Autowired
 	private BlogPostRepository blogPostRepository;
+	private static List<BlogPost> posts = new ArrayList<>();
 
+	// another way to write GetMapping is =
+	// @RequestMapping(method=RequestMethod.GET,"/")
 	@GetMapping(value = "/")
-	public String index(BlogPost blogPost) {
+	public String index(BlogPost blogPost, Model model) {
+		model.addAttribute("posts", posts);
 		return "blogpost/index";
 	}
+
+	// PUT
+	// POST
+	// DELETE
 
 	private BlogPost blogPost;
 
 	@PostMapping(value = "/")
 	public String addNewBlogPost(BlogPost blogPost, Model model) {
 		blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
+		posts.add(blogPost);
 		model.addAttribute("title", blogPost.getTitle());
 		model.addAttribute("author", blogPost.getAuthor());
 		model.addAttribute("blogEntry", blogPost.getBlogEntry());
